b65930cf1cf103b5bb7194039b67f4a75a54e927
the-supernova <biswalabhijit04@gmail.com>
2021-11-01 15:09:06 +0530

    Refactor code

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -10,21 +10,34 @@ from datetime import datetime
 # App Insights
 # TODO: Import required libraries for App Insights
 from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
 from opencensus.ext.azure import metrics_exporter
 from opencensus.stats import aggregation as aggregation_module
 from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
 from opencensus.stats import view as view_module
 from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
+#For metrics
+stats = stats_module.stats
+view_manager = stats.view_manager
+
 # Logging
 # TODO: Setup logger
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests'])
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=f4b338db-a621-461d-9958-410174cf01f9'))
+
+handler = AzureLogHandler(connection_string='InstrumentationKey=f4b338db-a621-461d-9958-410174cf01f9')
+handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+logger.addHandler(handler)
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=f4b338db-a621-461d-9958-410174cf01f9'))
+logger.setLevel(logging.INFO)
 
 # Metrics
 # TODO: Setup exporter
@@ -32,6 +45,7 @@ exporter = metrics_exporter.new_metrics_exporter(
     enable_standard_metrics=True,
     connection_string='InstrumentationKey=f4b338db-a621-461d-9958-410174cf01f9'
 )
+view_manager.register_exporter(exporter)
 
 # Tracing
 # TODO: Setup tracer
