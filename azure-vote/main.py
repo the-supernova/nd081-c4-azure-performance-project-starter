3bb36c0380c6fc133605338f8c26de6aaaf33de7
the-supernova <biswalabhijit04@gmail.com>
2021-11-01 11:59:31 +0530

    Fix logger issues

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -108,12 +108,12 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
-            logger.warning('Cats', extra=properties)
+            logger.info('Cats', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
-            logger.warning('Dogs', extra=properties)
+            logger.info('Dogs', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
